9d0ff11a1b7d4af29fe2a14fb416ef27530b9ba0
Thierry Meiers <meith0717@icloud.com>
2025-06-24 21:36:05 +0200

    Add star factory

---
+++ Gameplay/GameObjectFactory.cs
@@ -0,0 +1,29 @@
+﻿// GameObjectFactory.cs 
+// Copyright (c) 2023-2025 Thierry Meiers 
+// All rights reserved.
+
+using GameEngine.Content;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace GameEngine.Gameplay
+{
+    public abstract class GameObjectFactory<T>
+    {
+        protected readonly ContentContainer<T> ObjectPrefabs = new();
+        public readonly ImmutableArray<string> IDs;
+
+        public GameObjectFactory(ContentContainer<object> objectPrefabs) 
+        {
+            var ids = new ConcurrentBag<string>();
+            foreach (var kvp in objectPrefabs.Content) 
+            {
+                if (kvp.Value is not T TData) continue;
+                ObjectPrefabs.Add(kvp.Key, TData);
+                ids.Add(kvp.Key);
+            };
+            IDs = ids.ToImmutableArray();
+        }
+    }
+}
