9342abbd9f850e66f093dacef8998311b4e589a9
Meith0717 <meith0717@icloud.com>
2025-08-13 12:19:23 +0200

    Fix xml build error

---
+++ Extensions/ToupleExtensions.cs
@@ -0,0 +1,17 @@
+﻿// ToupleExtensions.cs 
+// Copyright (c) 2023-2025 Thierry Meiers 
+// All rights reserved.
+
+using Microsoft.Xna.Framework;
+
+namespace GameEngine.Extensions
+{
+    public static class ToupleExtensions
+    {
+        public static Vector2 ToVector2(this (float, float) touple)
+            => new(touple.Item1, touple.Item2);
+
+        public static Point ToPoint(this (int, int) touple)
+            => new(touple.Item1, touple.Item2);
+    }
+}
