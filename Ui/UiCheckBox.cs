911fb0790f6752045fca7f71dae192af3a0e6ddf
Meith0717 <meith0717@icloud.com>
2025-10-07 09:28:21 +0200

    Update UI & crash fix

--- Ui/UiCheckBox.cs
+++ Ui/UiCheckBox.cs
@@ -7,16 +7,20 @@ namespace GameEngine.Ui
     public sealed class UiCheckBox : UiButton.Sprite
     {
         private bool _state;
+        private readonly string _trueButton;
+        private readonly string _falseButton;
 
-        public UiCheckBox() : base("ui_toggle_true")
+        public UiCheckBox(string trueButton, string falseButton) : base(trueButton)
         {
             OnClickAction += () => State = !State;
+            _trueButton = trueButton;
+            _falseButton = falseButton;
         }
 
         public bool State
         {
             get => _state;
-            set => Texture = (_state = value) ? "ui_toggle_true" : "ui_toggle_false";
+            set => Texture = (_state = value) ? _trueButton : _falseButton;
         }
     }
 }
