f2b4793d7ac07520e0166e28c68d9bff11d148d2
Thierry Meiers <meith0717@icloud.com>
2026-01-31 20:41:58 +0100

    Bring factory back

---
+++ Gameplay/GameObjectFactory.cs
@@ -0,0 +1,15 @@
+﻿// GameObjectFactory.cs
+// Copyright (c) 2023-2026 Thierry Meiers
+// All rights reserved.
+// Portions generated or assisted by AI.
+
+using MonoKit.Content;
+
+namespace MonoKit.Gameplay
+{
+    public abstract class GameObjectFactory<T>(ContentContainer<object> contentProvider)
+        where T : class
+    {
+        protected readonly ContentContainer<T> ContentProvider = contentProvider.Cast<T>();
+    }
+}
